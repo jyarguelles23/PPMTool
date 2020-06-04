0d3eb776cab7af957da3b7e7eed17c063645bb19
jyarguelles23 <jyarguelles23@gmail.com>
2020-06-04 16:04:24 -0400

    Valid y Binding Result mostrando mensajes

--- PPMToolFullStack/src/main/java/kdc/developers/ppmtool/Controllers/ProjectController.java
+++ PPMToolFullStack/src/main/java/kdc/developers/ppmtool/Controllers/ProjectController.java
@@ -5,10 +5,17 @@ import kdc.developers.ppmtool.Services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/project")
@@ -18,8 +25,15 @@ public class ProjectController {
     ProjectService service;
 
     @PostMapping("")
-    public ResponseEntity<Project>  createNewProject(@RequestBody Project project){
-
+    public ResponseEntity<?>  createNewProject(@Valid @RequestBody Project project, BindingResult result){
+       if(result.hasErrors()){
+           List<FieldError> field=result.getFieldErrors();
+           HashMap<String,String> error_map=new HashMap<>();
+           for (FieldError error : result.getFieldErrors() ){
+               error_map.put(error.getField(),error.getDefaultMessage());
+           }
+           return new ResponseEntity<HashMap<String,String>>(error_map,HttpStatus.BAD_REQUEST);
+       }
        return  new ResponseEntity<Project>(service.saveOrUpdate(project), HttpStatus.CREATED);
     }
 
