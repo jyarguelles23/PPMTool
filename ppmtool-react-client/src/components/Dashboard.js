319129b63de78534e1cac1302c01aec323357191
jyarguelles23 <jyarguelles23@gmail.com>
2020-06-10 00:24:11 -0400

    React con rutas

---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+
+import CreateProjectButton from "./Project/CreateProjectButton";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        {
+          //<!-- Dashboard Component (Project Item included) -->
+        }
+        <div className="projects">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h1 className="display-4 text-center">Projects</h1>
+                <br />
+                <CreateProjectButton></CreateProjectButton>
+                <br />
+                <hr />
+
+                {
+                  //<!-- Project Item Component -->
+                }
+                <ProjectItem />
+                {
+                  // End of Project Item Component
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
