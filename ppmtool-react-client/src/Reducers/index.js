189b6dc476401c4967caa29c42ff281d97a9373f
jyarguelles23 <jyarguelles23@gmail.com>
2020-06-24 14:14:41 -0400

    folder structure projecttask

--- ppmtool-react-client/src/Reducers/index.js
+++ ppmtool-react-client/src/Reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import getProjectReducer from "./getProjectReducer";
+import backlogReducer from "./backLogReducer";
 export default combineReducers({
   //all the reducers we will creating
 
   errors: errorReducer,
   project: getProjectReducer,
+  backlog: backlogReducer,
 });
