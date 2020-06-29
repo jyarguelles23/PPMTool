08604cc1eb8fad128ca5509a30958869757be179
jyarguelles23 <jyarguelles23@gmail.com>
2020-06-29 13:30:13 -0400

    Rutas de projectBoard and AddProject Task

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -8,6 +8,8 @@ import Header from "./components/Layout/Header";
 import { Provider } from "react-redux";
 import store from "./Store";
 import UpdateProject from "./components/Project/UpdateProject";
+import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
+import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
 function App() {
   return (
     //provider es de redux y es el q permite conectar las stores con los appjs
@@ -19,6 +21,8 @@ function App() {
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/addProject" component={AddProject} />
           <Route exact path="/updateProject/:id" component={UpdateProject} />
+          <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+          <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
         </div>
       </Router>
     </Provider>
